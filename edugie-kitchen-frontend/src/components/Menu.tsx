ec9b845100e02681b1aa50d289042259e6bfe508
ambiti0n <fabulousfresh010@gmail.com>
2022-03-22 21:09:32 +0100

    line-spacing between food-title and price

--- edugie-kitchen-frontend/src/components/Menu.tsx
+++ edugie-kitchen-frontend/src/components/Menu.tsx
@@ -17,7 +17,7 @@ const Menu = ({ items }: { items: any }) => {
               <img src={img} alt={title} className="photo" />
               <div className="item-info">
                 <header>
-                  <h4>{title}</h4>
+                  <h4 className="food-title">{title}</h4> 
                   <h4 className="price">₦{price}</h4>
                 </header>
                 <p className="item-text">{desc}</p>
