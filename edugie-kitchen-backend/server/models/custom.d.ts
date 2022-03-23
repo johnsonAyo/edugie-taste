ffec84a04e80bde201bcc2826833b566c20c4086
ambiti0n <fabulousfresh010@gmail.com>
2022-03-23 01:04:42 +0100

    new update

--- edugie-kitchen-backend/server/models/custom.d.ts
+++
@@ -1,7 +0,0 @@
-import { IUser } from '../utils/interface';
-import { Request } from 'express';
-
-export interface CustomReq extends Request {
-  user?: Iuser 
-  file?: any
-}
--- edugie-kitchen-backend/server/utils/catchAsync.ts
+++
@@ -1,9 +0,0 @@
-import express, { NextFunction, Request, Response } from 'express';
-
-const catchAsync =  (fn: any) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch(next);
-  };
-};
-
-export default catchAsync
