460872604239c833e39b437431858b9b4aea8f5d
ambiti0n <fabulousfresh010@gmail.com>
2022-05-07 10:13:18 +0100

    cloudinary images

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,84 +7,84 @@ const Gallery = () => {
       <div className="gallery">
         <figure className="gallery-item">
           <img
-            src="img/gallery/gallery-1.jpg"
+            src="https://res.cloudinary.com/dfi24gqb6/image/upload/v1651914252/gallery-1_p5kem9.jpg"
             alt="Photo of beautifully
     arranged food"
           />
         </figure>
         <figure className="gallery-item">
           <img
-            src="img/gallery/gallery-2.jpg"
+            src="https://res.cloudinary.com/dfi24gqb6/image/upload/v1651914273/gallery-2_j2v4q1.jpg"
             alt="Photo of beautifully
     arranged food"
           />
         </figure>
         <figure className="gallery-item">
           <img
-            src="img/gallery/gallery-3.jpg"
+            src="https://res.cloudinary.com/dfi24gqb6/image/upload/v1651914304/gallery-3_bgd8h0.jpg"
             alt="Photo of beautifully
     arranged food"
           />
         </figure>
         <figure className="gallery-item">
           <img
-            src="img/gallery/gallery-4.jpg"
+            src="https://res.cloudinary.com/dfi24gqb6/image/upload/v1651914336/gallery-4_ja190v.jpg"
             alt="Photo of beautifully
     arranged food"
           />
         </figure>
         <figure className="gallery-item">
           <img
-            src="img/gallery/gallery-5.jpg"
+            src="https://res.cloudinary.com/dfi24gqb6/image/upload/v1651914365/gallery-5_wws4b9.jpg"
             alt="Photo of beautifully
     arranged food"
           />
         </figure>
         <figure className="gallery-item">
           <img
-            src="img/gallery/gallery-6.jpg"
+            src="https://res.cloudinary.com/dfi24gqb6/image/upload/v1651914390/gallery-6_vapdbg.jpg"
             alt="Photo of beautifully
     arranged food"
           />
         </figure>
         <figure className="gallery-item">
           <img
-            src="img/gallery/gallery-7.jpg"
+            src="https://res.cloudinary.com/dfi24gqb6/image/upload/v1651914528/gallery-12_vqkee4.jpg"
             alt="Photo of beautifully
     arranged food"
           />
         </figure>
         <figure className="gallery-item">
           <img
-            src="img/gallery/gallery-8.jpg"
+            src="https://res.cloudinary.com/dfi24gqb6/image/upload/v1651914440/gallery-8_opb2di.jpg"
             alt="Photo of beautifully
     arranged food"
           />
         </figure>
         <figure className="gallery-item">
           <img
-            src="img/gallery/gallery-9.jpg"
+            src="https://res.cloudinary.com/dfi24gqb6/image/upload/v1651914462/gallery-9_ezyuea.jpg"
             alt="Photo of beautifully
     arranged food"
           />
         </figure>
         <figure className="gallery-item">
           <img
-            src="img/gallery/gallery-10.jpg"
+            src="https://res.cloudinary.com/dfi24gqb6/image/upload/v1651914486/gallery-10_khg2js.jpg"
             alt="Photo of beautifully
     arranged food"
           />
         </figure>
         <figure className="gallery-item">
           <img
-            src="img/gallery/gallery-11.jpg"
+            src="https://res.cloudinary.com/dfi24gqb6/image/upload/v1651914505/gallery-11_p5ltgo.jpg"
             alt="Photo of beautifully
     arranged food"
           />
         </figure>
         <figure className="gallery-item">
           <img
-            src="img/gallery/gallery-12.jpg"
+            src="https://res.cloudinary.com/dfi24gqb6/image/upload/v1651914416/gallery-7_c8oe1a.jpg"
             alt="Photo of beautifully
     arranged food"
           />
